862c3b79f47639b441b7fd495a655f58b02886ab
Marwos97 <marwos97@gmail.com>
2020-06-23 16:38:02 -0300

    Agregando cosas

--- pagBootstrap/js/ajax.js
+++ pagBootstrap/js/ajax.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+
     $('#gatito3').click(function(){
         $('#principal').load('buy3.html')
+    })
+
+    $('#gatito2').click(function(){
+        $('#principal').load('buy2.html')
+    })
+
+    $('#gatito1').click(function(){
+        $('#principal').load('buy1.html')
     })
 });
