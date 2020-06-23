f9ab9e1fc457f07a070c5392d9e703c162c773d9
joevidal5 <62678280+joevidal5@users.noreply.github.com>
2020-06-23 18:58:53 -0300

    el quita boxer

--- pagBootstrap/js/ajax.js
+++ pagBootstrap/js/ajax.js
@@ -11,18 +11,13 @@ $(document).ready(function(){
     $('#gatito3').click(function(){
         $('#principal').load('buy3.html')
     })
-<<<<<<< HEAD
     $('#comprar').click(function(){
         $('#principal').load('successful_purchase.html')
     })
     $('#volverPagPrin').click(function(){
         window.location.reload();
     })
-=======
-
-    $('compraE').click(function(){
-        $('#principal').load('successful_purchase.html')
+    $('#faqs').click(function(){
+        $('#principal').load('faqs.html')
     })
-
->>>>>>> e157818922d30c58295419c05e4a40f0db8242e5
 });
