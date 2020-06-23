e157818922d30c58295419c05e4a40f0db8242e5
Marwos97 <marwos97@gmail.com>
2020-06-23 17:37:28 -0300

    actualizo

--- pagBootstrap/js/ajax.js
+++ pagBootstrap/js/ajax.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
 
-    $('#gatito3').click(function(){
-        $('#principal').load('buy3.html')
+    $('#gatito1').click(function(){
+        $('#principal').load('buy1.html')
     })
 
     $('#gatito2').click(function(){
         $('#principal').load('buy2.html')
     })
 
-    $('#gatito1').click(function(){
-        $('#principal').load('buy1.html')
+    $('#gatito3').click(function(){
+        $('#principal').load('buy3.html')
     })
+
+    $('compraE').click(function(){
+        $('#principal').load('successful_purchase.html')
+    })
+
 });
